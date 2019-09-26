db60ed1d89ae1d38d2237a27689be109ada55a5b
Roman Cherepov <cherepov@mercurydevelopment.com>
2019-09-26 18:25:39 +0400

    Added Tests and Validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "product is not valid without a unique title" do
+    # если у товара нет уникального названия, то он недопустим
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+    # уже было использовано
+  end
 end
